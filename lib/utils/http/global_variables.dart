d8219567d08d701c9cf5495b2a92a6e4fabd42bc
TimNet-codx <jesulayomitimze55@gmail.com>
2026-04-07 09:43:28 +0100

    update url

--- lib/utils/http/global_variables.dart
+++ lib/utils/http/global_variables.dart
@@ -1,1 +1,4 @@
-String uri = "http://192.168.1.91:3000";
+// String uri = "http://192.168.1.91:3000";
+// https://tm-store-backend-api.onrender.com
+
+String uri = "https://tm-store-backend-api.onrender.com";
